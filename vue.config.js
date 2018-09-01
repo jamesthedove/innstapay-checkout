fe7ad116cfbfc8ffa50f1abbddc81f877bca51f6
James Falade <faladeveloper@gmail.com>
2018-09-01 14:45:59 +0100

    update base url

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,5 @@
 // make sure you update `yourProjectName` with the name of your GitLab project
 
 module.exports = {
-    baseUrl: process.env.NODE_ENV === 'production'
-        ? '/web-payment-gateway/'
-        : '/'
+    baseUrl: '/'
 }
